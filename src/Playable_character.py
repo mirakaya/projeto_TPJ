e6933e260e8acd6b25274f677adeda55de2c0f89
mirakaya <mariajonas.sousa00@gmail.com>
2022-11-28 12:02:18 +0000

    Minor changes.

---
+++ src/Playable_character.py
@@ -0,0 +1,63 @@
+from enum import Enum
+
+from src.common import *
+from src.fsm_characters import *
+
+class Event(Enum):
+	REGULAR = 1,
+	POWERUP = 2,
+	DIE = 3
+
+class Normal(State):
+	def __init__(self):
+		super().__init__(self.__class__.__name__)
+
+	@classmethod
+	def update(cls, ant):
+		ant.power_down()
+
+class Powered(State):
+	def __init__(self):
+		super().__init__(self.__class__.__name__)
+
+	@classmethod
+	def update(cls, ant):
+		ant.power_up()
+
+class Dead(State):
+	def __init__(self):
+		super().__init__(self.__class__.__name__)
+
+	@classmethod
+	def enter(cls, ant):
+		ant.dead = True
+
+
+STATES = [Normal, Powered, Dead]
+
+TRANSITIONS = {
+	Event.REGULAR: [Transition(Powered, Normal)],
+	Event.POWERUP: [Transition(Normal, Powered)],
+	Event.DIE: [
+		Transition(Normal, Dead),
+	]
+}
+
+
+
+
+#-----end of transitions -----
+
+class Playable_character:
+
+	def __init__(self):
+		self.dead = False
+
+	def move(self):
+		pass
+
+	def jump(self):
+		pass
+
+	def update(self):
+		pass
